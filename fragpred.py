047a29ca98b794d33efe643631a8509f5f0b7324
Nisarg Shah <141799170+NisargRhino@users.noreply.github.com>
2024-10-07 00:43:12 -0700

    Update fragpred.py

--- fragpred.py
+++ fragpred.py
@@ -37,7 +37,8 @@ def calculate_properties(smiles):
     }
 
 def get_3d_structure(smiles):
-    response = requests.post('https://frag-predict-portal-2.onrender.com/get_3d_structure', json={'smiles': smiles})
+    #response = requests.post('https://frag-predict-portal-2.onrender.com/get_3d_structure', json={'smiles': smiles})
+    response = requests.post('/get_3d_structure', json={'smiles': smiles})
     if response.status_code == 200:
         return response.json().get('pdb')
     else:
